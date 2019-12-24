8f3a5e195ee653c86c260099dd673cb43e329a41
Jzero <07mrjzero@gmail.com>
2019-12-24 12:41:22 +0900

    2019_12_24
    PL/SQL
     . 명시적 커서와 FOR LOOP
     . 인자가 있는 명시적 커서
     . FOR LOOP에서 커서 인라인 선언
    
    PL/SQL 실습
     . PRO_3
     . PRO_3 SQL로 작성하기 
        1. rownum
        2. 분석함수 
        3. 그룹함수
     . PRO_4
     , INSERT SELECT 절로 부하 줄이기

---
+++ dt.sql
@@ -0,0 +1,14 @@
+ CREATE TABLE DT
+(	DT DATE);
+
+insert into dt
+select trunc(sysdate + 10) from dual union all
+select trunc(sysdate + 5) from dual union all
+select trunc(sysdate) from dual union all
+select trunc(sysdate - 5) from dual union all
+select trunc(sysdate - 10) from dual union all
+select trunc(sysdate - 15) from dual union all
+select trunc(sysdate - 20) from dual union all
+select trunc(sysdate - 25) from dual;
+
+commit;
